847cfdba46d651e0f607b2f3a59dcce74ec2fff5
Phong <tranloiphong2@gmail.com>
2017-07-10 05:36:02 +0700

    Add update product

--- Entity/Helper/ProductHelper.cs
+++ Entity/Helper/ProductHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,23 +13,77 @@ namespace Entity.Helper
     {
         public ICollection<Product> GetAllProduct()
         {
-            return db.Products.ToList<Product>();
+            return db.Products.ToList();
         }
 
         public ICollection<Product> GetProductByCaterory(int categoryId)
         {
             return db.Products
                 .Where(product => product.categoryID == categoryId)
-                .ToList<Product>();
+                .ToList();
         }
 
-        public bool CreateProduct(string name,int categoryID, Decimal unitPrice, Uri image)
+        public Product CreateProduct(string name, int categoryId, Decimal unitPrice, Uri image)
         {
-//            Product product = new Product
-//            {
-////                name = 
-//            };
+            try
+            {
+                Product product = new Product
+                {
+                    name = name,
+                    categoryID = categoryId,
+                    unitPrice = unitPrice,
+                    image = Path.GetFileName(image.AbsoluteUri)
+                };
+                db.Products.Add(product);
+                db.SaveChanges();
+                return product;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public bool UpdateProduct(int id, string name, int? categoryId, decimal? unitPrice, Uri image)
+        {
+            try
+            {
+                int affectedRecord = 0;
+                Product product = db.Products.Find(id);
+                if (product != null)
+                {
+                    if (name != null)
+                    {
+                        product.name = name;
+                    }
+                    if (categoryId != null)
+                    {
+                        product.categoryID = categoryId;
+                    }
+                    if (unitPrice != null)
+                    {
+                        product.unitPrice = unitPrice.Value;
+                    }
+                    if (image != null)
+                    {
+                        product.image = Path.GetFileName(image.AbsoluteUri);
+                    }
+
+                    affectedRecord = db.SaveChanges();
+                }
+                return affectedRecord == 1;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool RemoveProduct(int id)
+        {
             throw new NotImplementedException();
         }
+        
+        
     }
 }
